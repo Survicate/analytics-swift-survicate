71d8461e255acd7c6272786a036b176704c79157
Dariusz Konieczko <dariusz@survicate.com>
2025-07-24 10:09:04 +0200

    Set Survicate SDK version constraint from the major 6.0.0

--- Package.swift
+++ Package.swift
@@ -25,7 +25,7 @@ let package = Package(
         .package(
             name: "Survicate",
             url: "https://github.com/Survicate/survicate-ios-sdk",
-            from: "6.3.4"
+            from: "6.0.0"
         )
     ],
     targets: [
